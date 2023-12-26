e3dc857c406845d4b0b85faec5e917550a506172
Dmitriy <D1mk0Leb@yandex.ru>
2023-12-26 21:50:01 +0300

    Add index quality

--- test_gar_api_contract.py
+++ test_gar_api_contract.py
@@ -1,6 +1,8 @@
 import requests
 import pytest
 
+#from spellchecker import SpellChecker <-- Для проверки орфографии (Пока неактивно)
+
 BASE_URL = 'http://gar.gblinov.ru:18181'
 GET_URL = '/v1/Addresses'
 
@@ -21,20 +23,34 @@ address_base_set = [
     ['Москва, ул. Сухонская д 11, кв 2'],
     ['город М'],
     ['город Москва, улица Римского'],
+    ['город Москва, Старая Басманная'],
+    ['город Москва, Новая Басманная'],
 ]
+
+def count_matching_items(list1, list2):
+    return len(set(list1).intersection(list2))
 
 @pytest.mark.parametrize(
     argnames=['address'],
     argvalues=address_base_set
 )
-def test_should_be_api_contract(address):
-    payload = {'Name': f'{address}', 'Limit': '3'}
+def test_should_be_api_address_contract(address):
+    if address == 'город М':
+        pytest.xfail(f'Слишком уж некорректный адрес: {address}')
+        return
+    payload = {'Name': f'{address}', 'Limit': '10'}
     response = requests.get(f'{BASE_URL}{GET_URL}', params=payload)
     assert response.status_code == 200, \
-        f'Статус ответа не в порядке :{response.status_code}'
+        f'\nСтатус ответа не в порядке :{response.status_code}'
     response_json = response.json()
     response_address = response_json[0]['name']['default']
-
+    list_address = address.split()
+    list_response_address = response_address.split()
+    hint_quality_index = count_matching_items(list_address, list_response_address)
+    print(f'\nИндекс качества подсказки: {hint_quality_index}')
+    assert hint_quality_index >= 2, \
+        f'Для запроса: "{address}" - слишком низкий индекс качества подсказки = {hint_quality_index}. ' \
+        f'Была возвращена подсказка: {response_address}'
 
 if __name__ == "__main__":
     pytest.main([__file__])
