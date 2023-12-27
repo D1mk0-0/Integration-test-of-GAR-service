cc675be8e27a091f1d00d61facc5d8a00831f7ab
Dmitriy <D1mk0Leb@yandex.ru>
2023-12-27 09:25:23 +0300

    Add method get address

--- methods/base_method.py
+++ methods/base_method.py
@@ -1,2 +1,7 @@
 class BaseMethod():
-    assert True
+    def __init__(self, response):
+        self.response = response
+
+    def get_address_from_json(self):
+        self.address = self.response.json()
+        self.address.response_json[0]['name']['default']
