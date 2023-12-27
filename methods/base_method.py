f0317433c7efceb6eff0ae5e261a281f2e36fd38
Dmitriy <D1mk0Leb@yandex.ru>
2023-12-27 16:10:50 +0300

    refactoring README and comments

--- methods/base_method.py
+++ methods/base_method.py
@@ -4,7 +4,6 @@ from .connect_method import ConnectMethod
 
 class BaseMethod():
     def __init__(self, address):
-        #self.response = response
         self.address = address
 
     def send_get_request_with_address(self, address, limit):
@@ -21,13 +20,13 @@ class BaseMethod():
 
     def should_be_quality_index(self):
         response_json = self.response.json()
-        response_address = response_json[0]['name']['default']
+        response_first_hint = response_json[0]['name']['default']
         list_address = self.address.split()
-        list_response_address = response_address.split()
-        hint_quality_index = self.count_matching_items(list_address, list_response_address)
-        assert hint_quality_index >= 2, \
-            f'Для запроса: "{self.address}" - слишком низкий индекс качества подсказки = {hint_quality_index}. ' \
-            f'Была возвращена подсказка: {response_address}'
+        list_response_address = response_first_hint.split()
+        first_hint_quality_index = self.count_matching_items(list_address, list_response_address)
+        assert first_hint_quality_index >= 2, \
+            f'Для запроса: "{self.address}" - слишком низкий индекс качества первой подсказки = {first_hint_quality_index}. ' \
+            f'Была возвращена подсказка: {response_first_hint}'
 
 # assert response_address == re.compile(r"г\s*(\w+),\s*ул\s*(\w+),\s*д\.\s*(\d+)\s*кв\s*(\d+)"), \
 #    f'{response_address} не соответствует формату ответа.'
