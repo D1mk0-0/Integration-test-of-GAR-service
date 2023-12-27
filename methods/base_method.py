48d97742882f2a3d77a8ff1bd61dd9138e362f75
Dmitriy <D1mk0Leb@yandex.ru>
2023-12-27 12:19:58 +0300

    Used quality method

--- methods/base_method.py
+++ methods/base_method.py
@@ -1,7 +1,9 @@
+import requests
 
 class BaseMethod():
-    def __init__(self, response):
+    def __init__(self, response, address):
         self.response = response
+        self.address = address
 
     def get_address_from_json(self):
         address = self.response.json()
@@ -11,4 +13,22 @@ class BaseMethod():
     def should_be_status_code_200(self):
         assert self.response.status_code == 200, \
             f'\nСтатус ответа не в порядке :{self.response.status_code}'
+
+    def count_matching_items(self, list1, list2):
+        return len(set(list1).intersection(list2))
+
+    def should_be_quality_index(self):
+        response_json = self.response.json()
+        response_address = response_json[0]['name']['default']
+        list_address = self.address.split()
+        list_response_address = response_address.split()
+        hint_quality_index = self.count_matching_items(list_address, list_response_address)
+        assert hint_quality_index >= 2, \
+            f'Для запроса: "{self.address}" - слишком низкий индекс качества подсказки = {hint_quality_index}. ' \
+            f'Была возвращена подсказка: {response_address}'
+
+
+
+
+
 
--- test_gar_api_contract.py
+++ test_gar_api_contract.py
@@ -7,6 +7,7 @@ from data.address_data import AddressData
 
 #from spellchecker import SpellChecker <-- Для проверки орфографии ответов (Пока неактивно)
 
+
 def count_matching_items(list1, list2):
     return len(set(list1).intersection(list2))
 
@@ -20,17 +21,9 @@ def test_should_be_api_address_contract(address):
         return
     payload = {'Name': f'{address}', 'Limit': '10'}
     response = requests.get(f'{ConnectMethod.BASE_URL}{ConnectMethod.GET_URL_ADDRESS}', params=payload)
-    request = BaseMethod(response)
+    request = BaseMethod(response, address)
     request.should_be_status_code_200()
-    response_json = response.json()
-    response_address = response_json[0]['name']['default']
-    list_address = address.split()
-    list_response_address = response_address.split()
-    hint_quality_index = count_matching_items(list_address, list_response_address)
-    print(f'\nИндекс качества подсказки: {hint_quality_index}')
-    assert hint_quality_index >= 2, \
-        f'Для запроса: "{address}" - слишком низкий индекс качества подсказки = {hint_quality_index}. ' \
-        f'Была возвращена подсказка: {response_address}'
+    request.should_be_quality_index()
     #assert response_address == re.compile(r"г\s*(\w+),\s*ул\s*(\w+),\s*д\.\s*(\d+)\s*кв\s*(\d+)"), \
     #    f'{response_address} не соответствует формату ответа.'
 
